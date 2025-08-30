0cd99d91829ccc3bcbb4f33d9c52fe6168b9241d
Vikas gadariya <vikasgadariya07@gmail.com>
2025-08-30 10:03:37 +0530

    Class5

---
+++ Class5/script.js
@@ -0,0 +1,14 @@
+let arr = [4, 5, [3, 6, "Grow"], ['Ram', 100, "Ramayan", ["Shyam", "Radhey"]], "sunday"]
+console.log(arr[3][3][0])
+
+let arr1 = [100, 200, 500, 1000, { fname: "kallu", lname: "patel" }, ['Vikas', "Gadariya", "Dharukhedi"], ["indore",
+    {
+        city: "khandwa",
+        village: "aashapur"
+        
+    },
+    ["vineet", "arpit", "parth", "abhishek", ["Grow tech"]]]]
+
+console.log(arr1[6][1].village)
+console.log(arr1[6][2][0])
+console.log(arr1[6][2][4][0])

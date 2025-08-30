048afa9bec35c7353bad2209e1b35ed3b74bd2db
Vikas gadariya <vikasgadariya07@gmail.com>
2025-08-30 10:01:46 +0530

    Class2

---
+++ class2/script.js
@@ -0,0 +1,146 @@
+// 
+// function say(t,d){
+//     console.log("x,y", t,d);
+//     return  x + y ;
+// }
+  
+// let x=say(10,20)
+// console.log(x)
+
+// let a=15;
+// let b=20;
+// console.log(b<a);
+// console.log(typeof(b>a))
+// console.log(a & b)
+
+ 
+
+// function add(x,y){
+//       console.log(x+y)
+      
+// }
+// add(15,29)
+// console.log(add(35,60));
+// add(25,35)
+
+// let x=10;
+// {
+//       console.log(x)
+//       console.log(y)
+//       console.log(z)
+// 
+// }
+// // let x = 10;
+// const y = 12;
+// var z= 25;
+
+// for(i=1;i<=5;i++){
+//       console.log(i)
+// }
+
+
+// function sayHi() {
+//   if (true) {
+//     var message = "Hi!";
+//   }
+//   console.log(message);
+// }
+// sayHi();
+// sayHi();
+
+// {
+//       let x=5;
+//       const y= 10;
+// }
+// console.log(x)
+// console.log(y)
+
+
+// console.log(firstName)
+// firstName()
+// function firstName(){
+//       console.log("vikas gadariya")
+// }
+// console.log(firstName)
+// firstName()
+
+
+// function sayHello(){
+//   console.log("hello")
+
+// }
+// sayHello();
+
+// function multi(a,b){
+//       return a*b;
+      
+// }
+// let x=multi(25,35);
+// console.log(x)
+
+// function div(a,b){
+       
+//       return a/b;
+      
+// }
+// console.log(div(100,5))
+
+
+
+// function yourInfo(username,age){
+//       console.log("hello ",username, "welcome ", " your ",age,"is to low to attend these program");
+
+
+// }
+
+//  yourInfo("vikas",25)
+
+// // pure function
+// function call(){
+//       console.log("calll me ...")
+// }
+// call()
+// call()
+// call()
+// //  impure function
+// let v=1;
+// function add(){
+//       v++;
+//       console.log(v)
+    
+// }
+
+// add()
+// add()
+// add()
+// add()
+// add()
+
+
+
+// function callbackfun(){
+//       console.log("hello")
+// }
+
+// function hoc(a,b,fun){
+//       // fun(callbackfun)
+//       fun()
+       
+//       return a+b;
+// }
+
+// let sum=hoc(15,20,callbackfun)
+// console.log(sum)
+
+// function add(a,b){
+//       return a+b;
+// }
+//  let sum=add(15,23);
+//  console.log(add)
+//  console.log(sum)
+ 
+
+
+
+      
+       
